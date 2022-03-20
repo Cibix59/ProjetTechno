f85ed75a00c82b8ebd7f5b7d523a39566eff78e5
Clément Bourdier <99341714+Cibix59@users.noreply.github.com>
2022-03-20 09:20:27 -0400

    fix : nettoyage du code (#2)

--- API/routes/historique.js
+++ API/routes/historique.js
@@ -1,78 +1,51 @@
 const express = require('express');
-const historique = require('../models/historique');
 const router = express.Router();
 //const user = require('../controller/peripheriques');
 //const auth = require('../middleware/auth');
-const unhistorique = require('../models/historique');
+const Historique = require('../models/historique');
 
 router.get('/:nom', getUnHistorique, (req, res) => {
     console.log(res.lhistorique)
     res.send(res.lhistorique)
 })
 
-//Récupère tous les noms des bois avec leurs id
-router.get('/senseur', async (req, res) => {
-    try {
-        const senseur = await unhistorique.find(
-            {},
-            {
-                _id: 1,
-            }
-        )
-        res.json(senseur)
-    } catch (err) {
-        res.status(500).json({ message: err.message })
-    }
+router.get('/:nom', getUnHistorique, (req, res) => {
+    console.log(res.lhistorique)
+    res.send(res.lhistorique)
 })
-
 
 
 //Ajout d'un nouvel enregistrement
 router.post('/log', async (req, res) => {
-    console.log( req.body)
-    console.log( req.body.date)
-    unhistorique = new historique({
+
+    console.log(req.body.date)
+    console.log(req.body.heure)
+    console.log(req.body.evennement)
+    console.log(req.body.nomPeripherique)
+    console.log(req.body.descriptionPeripherique)
+    unhistorique = new Historique({
         date: req.body.date,
         heure: req.body.heure,
         evennement: req.body.evennement,
-        peripherique: [{ nom: req.body.nomPeripherique, description: req.body.descriptionDeripherique }]
+        nomPeripherique: req.body.nomPeripherique,
+        descriptionPeripherique: req.body.descriptionPeripherique
     })
 
-    try{
+    try {
         const nouvelHistorique = await unhistorique.save()
         res.status(201).json(nouvelHistorique)
     }
-    catch(err){
-        res.status(400).json({message: err.message})
+    catch (err) {
+        res.status(400).json({ message: err.message })
     }
 })
 
-
-/* router.get('/:nom', (req, res) => {
-    let paramBois = upperFirstLetter(req.params.bois);
-    Bois.findByWood(paramBois, (err, data) => {
-        if (err) {
-            if (err.kind === "not_found") {
-                res.status(404).send({
-                    message: `Le bois avec le nom ${req.params.bois} n'a pas été trouvé.`
-                });
-            } else {
-                res.status(500).send({
-                    message: "Erreur lors de l'obtention du bois ayant le nom " + req.params.bois
-                });
-            }
-        } else res.send(data);
-    });
-}) */
-
-
-
-
+//Donne l'historique d'un peripherique
 async function getUnHistorique(req, res, next) {
     let lhistorique
     //donnees = '"peripherique.nom":"'+req.params.name+'"';
     try {
-        lhistorique = await unhistorique.find({ "peripherique.nom":req.params.nom })
+        lhistorique = await Historique.find({ "nomPeripherique": req.params.nom })
         if (lhistorique == null) {
             return res.status(404).json({ message: "Impossible de trouver le peripherique" })
         }
